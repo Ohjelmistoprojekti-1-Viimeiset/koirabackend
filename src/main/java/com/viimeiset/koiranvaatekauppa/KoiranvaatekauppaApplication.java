2940af6d0bcb9d54616600d923691221b77711e7
Tuukka Grönholm <tmgronholm@gmail.com>
2023-10-25 12:07:36 +0300

    first commit

---
+++ src/main/java/com/viimeiset/koiranvaatekauppa/KoiranvaatekauppaApplication.java
@@ -0,0 +1,13 @@
+package com.viimeiset.koiranvaatekauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiranvaatekauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoiranvaatekauppaApplication.class, args);
+	}
+
+}
