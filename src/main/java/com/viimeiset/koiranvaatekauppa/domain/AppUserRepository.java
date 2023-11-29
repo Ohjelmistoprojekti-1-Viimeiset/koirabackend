4efbd2d598055b009e5d85c8eba284434161d125
Juho14 <juqiqekli@gmail.com>
2023-11-29 13:06:12 +0200

    Repo palauttaa listan yhhden olion sijaan.

--- src/main/java/com/viimeiset/koiranvaatekauppa/domain/AppUserRepository.java
+++ src/main/java/com/viimeiset/koiranvaatekauppa/domain/AppUserRepository.java
@@ -1,8 +1,10 @@
 package com.viimeiset.koiranvaatekauppa.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
 	AppUser findByUsername(String username);
-	AppUser findByRole(String role);
+	List<AppUser> findByRole(String role);
 }
