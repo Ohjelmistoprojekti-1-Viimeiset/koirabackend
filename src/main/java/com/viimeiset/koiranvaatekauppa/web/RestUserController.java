2179b913cbf88daab10dd5f12e9bcf813d13b227
Juho14 <juqiqekli@gmail.com>
2023-12-01 15:46:53 +0200

    DeleteMapping RestUserControllerissa

--- src/main/java/com/viimeiset/koiranvaatekauppa/web/RestUserController.java
+++ src/main/java/com/viimeiset/koiranvaatekauppa/web/RestUserController.java
@@ -3,7 +3,9 @@ package com.viimeiset.koiranvaatekauppa.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,12 @@ public class RestUserController {
 		userRepository.save(newUser);
 		return ResponseEntity.status(HttpStatus.CREATED).body(user);
 	}
-
+	
+	@DeleteMapping
+	public ResponseEntity<Void> poistaKayttaja(@PathVariable Long userId) {
+		AppUser user = userRepository.findById(userId).get();
+		userRepository.delete(user);
+		return ResponseEntity.noContent().build();
+		
+	}
 }
