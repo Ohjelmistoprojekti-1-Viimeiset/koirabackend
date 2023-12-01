a7a12b669d26c8952ad4e496c8559766049828d6
Tuukka Grönholm <tmgronholm@gmail.com>
2023-12-01 19:57:56 +0200

    tuotetest kommentointia lisätty

--- src/test/java/com/viimeiset/koiranvaatekauppa/TuoteTest.java
+++ src/test/java/com/viimeiset/koiranvaatekauppa/TuoteTest.java
@@ -34,10 +34,12 @@ public class TuoteTest {
 		// Testi tuotteen lisäämiseen
 		@Test
 		public void addTuote() {
+		// luodaan uusi tuote
 		Tuote tuote = new Tuote("Nappitakki", Tyyppi.VAATE, "Musta", Koko.M, 29.99,
 				valmistajaRepository.findByNimi("Northface"), 10);
+		// tallennetaan repositoryn avulla uusi tuote
 		repository.save(tuote);
-		
+		// testataan löytyykö uusi tuote tietokannasta
 		assertThat(tuote.getId()).isNotNull();
 			
 		}
@@ -47,15 +49,15 @@ public class TuoteTest {
 		
 		@Test 
 		public void removeTuote() {
+		// tuodaan listaan tuote jonka teidetään olevan tietokanassa
 		List<Tuote> tuotteet = repository.findByTuoteNimi("Takki");
 		Tuote tuote = tuotteet.get(0);
-		
+		// poistetaan tuote repositoryn avulla
 		repository.delete(tuote);
-		
+		// koitetaan tuoda poistrettu tuote uuteen listaan
 		List<Tuote> newTuotteet = repository.findByTuoteNimi("Takki");
-		
+		// jos tuotteen poisto on onnistunut uuden listan koko on nolla
 		assertThat(newTuotteet).hasSize(0);
-		
 		
 		}
 		
